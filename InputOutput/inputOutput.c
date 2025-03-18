89077b2f61ce9464770ffc9ac40644547978c0b4
bearibeiroa <contato.biaalves@gmail.com>
2025-03-17 22:54:23 -0300

    input output em c

---
+++ InputOutput/inputOutput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 38;
+    float altura = 1.75;
+    double saldo = 35603.55;
+    char inicial = 'B';
+    char nome[10] = "Bea";
+
+    printf("Idade - %d anos \n", idade);
+
+}
