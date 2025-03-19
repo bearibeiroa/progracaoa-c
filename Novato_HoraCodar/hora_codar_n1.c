fb0f8f06105a3249440b438008f319e0ad1f2541
Beatriz Ribeiro <contato.biaalves@gmail.com>
2025-03-19 12:45:44 +0000

    define ponto flutuante para floar

--- Novato_HoraCodar/hora_codar_n1.c
+++ Novato_HoraCodar/hora_codar_n1.c
@@ -12,13 +12,17 @@ int main() {
     scanf("%f", &altura);
         
     printf("Digite seu nome: \n");
-    scanf("%s", &nome);
+    scanf("%s", nome);
     
     printf("Digite sua matrícula: \n");
     scanf("%d", &matricula);
 
-    printf("Nome do aluno: %s \n Matrícula: %d \n", nome, matricula);
-    printf("Idade: %d \n Altura: %f \n", idade, altura);
+    printf("Nome do aluno: %s\nMatrícula: %d\n", nome, matricula);
+    printf("Idade: %d\nAltura: %.2f\n", idade, altura);
+
+    // Print tabulado
+    printf("Nome aluno\tMatrícula\tIdade\tAltura\n");
+    printf("%s\t%d\t%d\t%.2f", nome,matricula, idade, altura);
 
     return 0;
 
